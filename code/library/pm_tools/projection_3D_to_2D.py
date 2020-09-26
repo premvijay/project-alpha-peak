65c29b80ad43b86c7cebb281cb176726b0b2dde8
cprem <premv@iucaa.in>
2020-09-26 19:20:27 +0530

    find thick slice average

---
+++ code/library/pm_tools/projection_3D_to_2D.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+def average_2D_slice(grid, box_size, axis, around_position, thick):
+    start = (around_position[axis] - thick/2) * grid.shape[axis] // box_size
+    stop = (around_position[axis] + thick/2) * grid.shape[axis] // box_size
+    print(start,stop)
+    idx_range = np.arange(start,stop,dtype='int') % grid.shape[axis]
+    idx_range_3D = tuple([slice(None)]* axis + [idx_range])
+    print(idx_range_3D)
+    return grid[idx_range_3D].mean(axis=axis)
