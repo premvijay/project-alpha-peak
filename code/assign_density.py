8e28e09827be3c08e69760fe371a4fdb148b63af
cprem <premv@iucaa.in>
2020-09-27 16:17:55 +0530

    changed to GNU parallel

--- code/assign_density.py
+++ code/assign_density.py
@@ -18,7 +18,7 @@ parser = argparse.ArgumentParser(
     usage= 'python assign_density.py')
 
 parser.add_argument('--snapdir', type=str, help='Directory containing the snapshot binaries')
-parser.add_argument('--range', type=int, nargs=3, help='Range of snapshots to choose')
+parser.add_argument('--snap_i', type=int, help='Snapshot index number')
 
 parser.add_argument('--scheme', type=str, help='Scheme for assigning particles to grid')
 parser.add_argument('--grid_size', type=int, help='Grid size : number of cells along each direction')
@@ -34,15 +34,15 @@ comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 print('My rank is ', rank, 'Hostname is', socket.gethostname() )
 
-snapshots_to_run = np.arange(args.range[0],args.range[1],args.range[2], dtype=int)
+# snapshots_to_run = np.arange(args.range[0],args.range[1],args.range[2], dtype=int)
 
-snapshot_number_this_process = snapshots_to_run[rank]
+# snapshot_number_this_process = snapshots_to_run[rank]
 # snapshot_number = sys.argv[2]
 
 t_now = time()
 print('\n Starting to read snapshots binaries')
 
-filename_prefix = '/snapshot_{0:03d}'.format(snapshot_number_this_process)
+filename_prefix = '/snapshot_{0:03d}'.format(args.snap_i)
 filepath_prefix = args.snapdir + filename_prefix
 
 posd = read_positions_all_files(filepath_prefix)
@@ -60,18 +60,18 @@ snap.from_binary(filepath)
 delta = assign_density(posd, snap.box_size, args.grid_size, scheme=args.scheme)
 del posd
 
-print('\n Density assigned for snapshot {0:03d}'.format(snapshot_number_this_process))
+print('\n Density assigned for snapshot {0:03d}'.format(args.snap_i))
 t_bef, t_now = t_now, time()
 print(t_now-t_bef)
 
 if args.slice2D:
     mmhpos = (48.25266, 166.29897, 98.36325)
     delta_slice = project_to_slice(delta, snap.box_size, axis=2, around_position=mmhpos, thick=10)
-    np.save(args.outdir+'/slice2D/'+'/slice_{0:03d}.npy'.format(snapshot_number_this_process), delta_slice)
+    np.save(args.outdir+'/slice2D/'+'/slice_{0:03d}.npy'.format(args.snap_i), delta_slice)
 
 if args.Pk:
     power_spec = compute_power_spec(delta,snap.box_size)
-    filepath = args.outdir+'/power_spectrum/'+'Pk_{0:03d}.csv'.format(snapshot_number_this_process)
+    filepath = args.outdir+'/power_spectrum/'+'Pk_{0:03d}.csv'.format(args.snap_i)
     power_spec.to_csv(filepath, sep='\t', index=False, 
                             float_format='%.8e', header=['k (h/Mpc)', 'P(k) (Mpc/h)^3'])
 
