8a9b9d01bb2b1c7e77b880d66ee7375f429bb38d
cprem <premv@iucaa.in>
2020-09-28 00:06:21 +0530

    Time evolution video is made

--- code/non-linear_power_spec.py
+++ code/non-linear_power_spec.py
@@ -1,16 +1,63 @@
+import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation
+import pandas as pd
+import argparse
+import pdb
+
+parser = argparse.ArgumentParser(
+    description='Animation of power spectrum.',
+    usage= 'python ./non-linear_power_spec.py --Pkdir ~/myscratch/bdm_cdm1024/power_spectrum/')
+
+parser.add_argument('--Pkdir', type=str, help='Directory containing the saved power spectrum csv')
 
 
+args = parser.parse_args()
+
+i = 0
+power_spec = pd.read_csv(args.Pkdir+'/Pk_{0:03d}.csv'.format(i), sep='\t')
+power_spec.columns = ['k', 'Pk']
 
 
-fig2, ax2 = plt.subplots(1,1)
-ax2.plot(power_spec['k'],power_spec['Pk'])
+power_spec_grouped1 = power_spec.groupby(pd.cut(power_spec['k'], bins=np.linspace(power_spec['k'].iloc[1],power_spec['k'].iloc[-10], 200))).mean()
+power_spec_grouped2 = power_spec.groupby(pd.cut(power_spec['k'], bins=np.logspace(-2,1.3, 300))).mean()
+# pdb.set_trace()
+
+fig2, ax2 = plt.subplots(dpi=120)
+plot2 = ax2.plot(power_spec_grouped1['k'],power_spec_grouped1['Pk'])[0]
 # ax2[1].plot(power_spec['lam'],power_spec['Pk'])
 ax2.set_xlabel(r"$k ~~(h~\mathrm{Mpc}^{-1}$)")
 ax2.set_ylabel(r"$P(k) ~~(h^{-1}\mathrm{Mpc})^3$")
-
-
 ax2.set_xscale('log')
 ax2.set_yscale('log')
+ax2.set_ylim(top=1e5)
+ax2.grid(True)
 
-fig2.savefig('power_spec_{0:03d}.pdf'.format(snapshot_number))
+fig2.savefig(args.Pkdir+'/power_spec_{0:03d}.pdf'.format(0))
+
+
+def update(i):
+    power_spec = pd.read_csv(args.Pkdir+'Pk_{0:03d}.csv'.format(i), sep='\t')
+    power_spec.columns = ['k', 'Pk']
+    power_spec_grouped1 = power_spec.groupby(pd.cut(power_spec['k'], bins=np.linspace(power_spec['k'].iloc[1],power_spec['k'].iloc[-1], 200))).mean()
+    plot2.set_xdata(power_spec_grouped1['k'])
+    plot2.set_ydata(power_spec_grouped1['Pk'])
+    ax2.set_title("Non-linear power spectrum from {}th snapshot.".format(i))
+
+
+anim = matplotlib.animation.FuncAnimation(fig2, update, frames=201, interval=200)
+
+anim.save(args.Pkdir+"/non-linear_power_spectrum.mp4")#, writer="imagemagick")
+print("saved")
+
+
+
+
+
+
+
+
+
+
+
+
--- code/visualize_evolution_on_slice.py
+++ code/visualize_evolution_on_slice.py
@@ -1,14 +1,44 @@
+import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
+import matplotlib.animation
+import argparse
+import pdb
+
+parser = argparse.ArgumentParser(
+    description='Density field evolution from Gadgat simulation.',
+    usage= 'python ./visualize_evolution_on_slice.py --slicedir ~/myscratch/bdm_cdm1024/slice2D/')
+
+parser.add_argument('--slicedir', type=str, help='Directory containing the saved power spectrum csv')
+
+
+args = parser.parse_args()
+box_size =200
+
+i = 150
+
+delta_slice = np.load(args.slicedir+'/slice_{0:03d}.npy'.format(i))
+
 
 fig1, ax1 = plt.subplots(dpi=100)
-im1 = ax1.imshow(delta_slice+1, extent=[0,snap.box_size,0,snap.box_size], cmap='inferno', norm=LogNorm())
+im1 = ax1.imshow(delta_slice+1, extent=[0,box_size,0,box_size], cmap='inferno', norm=LogNorm())
 cb1 = fig1.colorbar(im1,ax=ax1)
 cb1.set_label(r"$(1+\delta)$")
-ax1.set_title("Density field assigned from the particle positions in the snapshot")
+ax1.set_title("2D slice of density field from the {:d}th snapshot".format(i))
 ax1.set_xlabel("$h^{-1}$Mpc")
 ax1.set_ylabel("$h^{-1}$Mpc")
 # ax1.set_xscale('log')
 
 
-fig1.savefig('delta2d.pdf', bbox_inchex='tight')
+fig1.savefig(args.slicedir+'delta2d.pdf', bbox_inchex='tight')
+
+def update2D(i):
+    delta_slice = np.load(args.slicedir+'/slice_{0:03d}.npy'.format(i))
+    im1.set_data(delta_slice+1)
+    ax1.set_title("2D slice of density field from the {:d}th snapshot".format(i))
+
+
+anim = matplotlib.animation.FuncAnimation(fig1, update2D, frames=201, interval=200)
+
+anim.save(args.slicedir+"/density_evolution_on_slice.mp4")#, writer="imagemagick")
+print("saved")
