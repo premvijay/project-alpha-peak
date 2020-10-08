1bab0a08041c78ed2a88f149c441b53a4b9097cb
cprem <premv@iucaa.in>
2020-10-09 00:33:11 +0530

    add a flag to give raw particle density

--- code/library/pm_tools/density_assignment.py
+++ code/library/pm_tools/density_assignment.py
@@ -3,7 +3,7 @@
 from . import particles_to_grid
 import numpy as np
 
-def assign_density(posd, box_size, grid_size = 512, scheme='CIC', shift=0):
+def assign_density(posd, box_size, grid_size = 512, scheme='CIC', shift=0, overdensity=True):
     # posd *= grid_size / box_size    # This happens without using extra memory
     # posd += shift
     posd_scaled_to_grid_units = posd * grid_size / box_size + shift
@@ -17,8 +17,12 @@ def assign_density(posd, box_size, grid_size = 512, scheme='CIC', shift=0):
         particle_grid = particles_to_grid.assign_tsc(posd_scaled_to_grid_units, grid_size)
     else:
         raise NotImplementedError('Requested scheme is unknown or not yet implemented')
-    density_grid = particle_grid
-    overdensity_grid = density_grid / (np.mean(density_grid)+1e-5) - 1
+    
     # print(density_grid)
-    return overdensity_grid#, particle_grid
+    if overdensity:
+        density_grid = particle_grid
+        overdensity_grid = density_grid / (np.mean(density_grid)+1e-5) - 1
+        return overdensity_grid
+    else:
+        return particle_grid
     
