9315bff196c72ac402b46b9032fde722726cd111
cprem <premv@iucaa.in>
2020-10-09 00:36:16 +0530

    flushed print

--- code/library/pm_tools/projection_3D_to_2D.py
+++ code/library/pm_tools/projection_3D_to_2D.py
@@ -4,8 +4,8 @@ import numpy as np
 def average_2D_slice(grid, box_size, axis, around_position, thick):
     start = (around_position[axis] - thick/2) * grid.shape[axis] // box_size
     stop = (around_position[axis] + thick/2) * grid.shape[axis] // box_size
-    print(start,stop)
+    print(start,stop, flush=True)
     idx_range = np.arange(start,stop,dtype='int') % grid.shape[axis]
     idx_range_3D = tuple([slice(None)]* axis + [idx_range])
-    print(idx_range_3D)
+    print(idx_range_3D, flush=True)
     return grid[idx_range_3D].mean(axis=axis)
