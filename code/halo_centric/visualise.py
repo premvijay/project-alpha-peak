182ed95bc673a878d7812d8ab80395fb355994f3
cprem <premv@iucaa.in>
2020-10-09 18:21:06 +0530

    metadata saved and used in plotting

--- code/halo_centric/visualise.py
+++ code/halo_centric/visualise.py
@@ -1,5 +1,5 @@
 import os
-import pickle
+import pickle, json
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
@@ -24,7 +24,7 @@ rundir = 'r1'
 
 simname = 'bdm_cdm1024' if args.simname is None else args.simname
 
-savesdir_global = os.path.join('/scratch/cprem/sims', simname, rundir, scheme, '{0:d}'.format(grid_size))
+savesdir_global = os.path.join('/scratch/cprem/sims', simname, rundir, 'TSC', '512')
 
 savesdir = os.path.join('/scratch/cprem/sims', simname, rundir, 'halo_centric', scheme, '{0:d}'.format(grid_size))
 
@@ -36,28 +36,29 @@ infodir = os.path.join(savesdir_global,'info')
 plotsdir = os.path.join(savesdir, 'plots_and_anims')
 os.makedirs(plotsdir, exist_ok=True)
 
-i = 150
+i = 9
 
 with open(os.path.join(infodir, 'header_{0:03d}.p'.format(i)), 'rb') as infofile:
     snap=pickle.load(infofile)
 
+with open( os.path.join(slicedir, 'slice_{0:03d}.meta'.format(i)), 'r' ) as metafile:
+    metadict = json.load(metafile)
 
-
-box_size = 3.8
+box_size = metadict['L_cube']
 
 
 
 delta_slice = np.load( os.path.join(slicedir, 'slice_{0:03d}.npy'.format(i)) )
 
 
-fig1, (ax1) = plt.subplots(figsize=(13,7), dpi=150)
+fig1, (ax1) = plt.subplots(figsize=(9,7), dpi=150)
 
 fig1.suptitle("Snapshot-{0:03d} at redshift z={1:.4f};     Simulation: {2}, Grid size: {3}, Scheme: {4}".format(i,snap.redshift,simname,grid_size,scheme))
 
-im1 = ax1.imshow(delta_slice+1, extent=[0,box_size,0,box_size], cmap='inferno', norm=LogNorm())
+im1 = ax1.imshow(delta_slice+1+1e-5, extent=[-box_size/2,box_size/2,-box_size/2,box_size/2], cmap='nipy_spectral', norm=LogNorm(vmin=3e-1))
 cb1 = fig1.colorbar(im1,ax=ax1)
 cb1.set_label(r"$(1+\delta)$")
-ax1.set_title(r"Halo centric stacked 0.5 $h^{-1}$ Mpc thick slice")
+ax1.set_title(r"0.25 $h^{-1}$ Mpc thick slice in halo centric stack of "+'{}'.format(metadict['N_stack']))
 ax1.set_xlabel(r"$h^{-1}$Mpc")
 ax1.set_ylabel(r"$h^{-1}$Mpc")
 # ax1.set_xscale('log')
@@ -65,26 +66,26 @@ ax1.set_ylabel(r"$h^{-1}$Mpc")
 
 
 
-fig1.savefig(os.path.join(plotsdir, 'single_snapshot.pdf'), bbox_inchex='tight')
+fig1.savefig(os.path.join(plotsdir, 'single_snapshot_{0:03d}.pdf'.format(i)), bbox_inchex='tight')
 
-def update(i):
-    print(i, 'starting')
-    delta_slice = np.load( os.path.join(slicedir, 'slice_{0:03d}.npy'.format(i)) )
-    im1.set_data(delta_slice+1)
+# def update(i):
+#     print(i, 'starting')
+#     delta_slice = np.load( os.path.join(slicedir, 'slice_{0:03d}.npy'.format(i)) )
+#     im1.set_data(delta_slice+1)
 
-    with open(os.path.join(infodir, 'header_{0:03d}.p'.format(i)), 'rb') as infofile:
-        snap=pickle.load(infofile)
-    fig1.suptitle("Snapshot-{0:03d} at redshift z={1:.4f};     Simulation: {2}, Grid size: {3}, Scheme: {4}".format(i,snap.redshift,simname,grid_size,scheme))
-    print(i,'stopping')
+#     with open(os.path.join(infodir, 'header_{0:03d}.p'.format(i)), 'rb') as infofile:
+#         snap=pickle.load(infofile)
+#     fig1.suptitle("Snapshot-{0:03d} at redshift z={1:.4f};     Simulation: {2}, Grid size: {3}, Scheme: {4}".format(i,snap.redshift,simname,grid_size,scheme))
+#     print(i,'stopping')
 
 
-anim = matplotlib.animation.FuncAnimation(fig1, update, frames=range(9,201), interval=500)
+# anim = matplotlib.animation.FuncAnimation(fig1, update, frames=range(9,201), interval=500)
 
-# plt.rcParams['animation.ffmpeg_path'] = ''
+# # plt.rcParams['animation.ffmpeg_path'] = ''
 
-# Writer=matplotlib.animation.ImageMagickWriter
-Writer=matplotlib.animation.FFMpegWriter
-writer = Writer(fps=10)
+# # Writer=matplotlib.animation.ImageMagickWriter
+# Writer=matplotlib.animation.FFMpegWriter
+# writer = Writer(fps=10)
 
-anim.save(os.path.join(plotsdir, 'simulation_visualisation.mp4'), writer=writer, dpi=150)
-print("saved")
+# anim.save(os.path.join(plotsdir, 'simulation_visualisation.mp4'), writer=writer, dpi=150)
+# print("saved")
