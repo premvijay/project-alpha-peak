3168c8b01ceec6bd582cb1e9e035b144e2b0a4c2
cprem <premv@iucaa.in>
2020-10-30 16:21:51 +0530

    changed the compile script to version agnostic and compiled for python 3.9

--- code/library/pm_tools/compile_module.sh
+++ code/library/pm_tools/compile_module.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd ~/project-alpha-peak/code/library/pm_tools/
 pwd
+# alias python3-config='python3.7m-config'
 export pybind_include=./src/pybind11/include/
-g++ -O3 -Wall -shared -std=c++11 -fPIC `python3.7m-config --includes` \
--I $pybind_include ./src/grid_assign_pybind11.cpp -o ./particles_to_grid`python3.7m-config --extension-suffix`
+python3-config --includes
+g++ -O3 -Wall -shared -std=c++11 -fPIC `python3-config --includes` -I $pybind_include ./src/grid_assign_pybind11.cpp -o ./particles_to_grid`python3-config --extension-suffix`
