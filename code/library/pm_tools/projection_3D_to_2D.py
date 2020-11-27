4862a93db7b26b33fd5dd2ac187ffea976db2c25
cprem <premv@iucaa.in>
2020-11-27 15:50:11 +0530

    bottleneck removed by using slice object for the special case of centre projection

--- code/library/pm_tools/projection_3D_to_2D.py
+++ code/library/pm_tools/projection_3D_to_2D.py
@@ -1,11 +1,39 @@
 import numpy as np
+from time import time
+import sys
 
 
 def average_2D_slice(grid, box_size, axis, around_position, thick):
-    start = (around_position[axis] - thick/2) * grid.shape[axis] // box_size
-    stop = (around_position[axis] + thick/2) * grid.shape[axis] // box_size
-    # print(start,stop, flush=True)
-    idx_range = np.arange(start,stop,dtype='int') % grid.shape[axis]
+    t_now = time()
+    if around_position == 'centre':
+        start = round((1/2 - thick/2 / box_size) * grid.shape[axis])
+        stop = round((1/2 - thick/2 / box_size) * grid.shape[axis])
+        idx_range = slice(start,stop) 
+    else:
+        start = (around_position[axis] - thick/2) * grid.shape[axis] // box_size
+        stop = (around_position[axis] + thick/2) * grid.shape[axis] // box_size
+        # print(start,stop, flush=True)
+        idx_range = np.arange(start,stop,dtype='int') % grid.shape[axis]
+
     idx_range_3D = tuple([slice(None)]* axis + [idx_range])
     # print(idx_range_3D, flush=True)
+    t_bef, t_now = t_now, time()
+    print("\n    slice index obtained")
+    print(t_now-t_bef)
+
     return grid[idx_range_3D].mean(axis=axis)
+
+    # grid_select = grid[idx_range_3D]
+    # t_bef, t_now = t_now, time()
+    # print("    thich slice grid selected")
+    # print(t_now-t_bef)
+    # grid_select = grid[:,:,idx_range]
+    # t_bef, t_now = t_now, time()
+    # print("    thich slice grid selected simply")
+    # print(t_now-t_bef)
+    # grid2D = grid_select.mean(axis=axis)
+    # t_bef, t_now = t_now, time()
+    # print("    thick slice pressed")
+    # print(t_now-t_bef)
+    # sys.stdout.flush()
+    # return grid2D
