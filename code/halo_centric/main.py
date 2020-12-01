bac3d97e7cf51e8254389df7ace115af7634d8e2
cprem <premv@iucaa.in>
2020-12-02 01:56:20 +0530

    make unaligned and aligned stacks

--- code/halo_centric/main.py
+++ code/halo_centric/main.py
@@ -78,7 +78,7 @@ if not args.align and not args.noalign:
 
 snapdir = os.path.join(args.simdir, args.simname, args.rundir)
 halosfile = os.path.join(args.outdir, args.simname, args.rundir, 'halo_centric', 'halos_list', f'halos_select_{args.M_around:.1e}_{args.max_halos:d}{args.halos_file_suffix:s}.csv')
-halosfile = os.path.join('/scratch/cprem/sims', args.simname, args.rundir, 'halo_centric', 'halos_list', f'halos_select_{args.M_around:.1e}_{args.max_halos:d}{args.halos_file_suffix:s}.csv')
+# halosfile = os.path.join('/scratch/cprem/sims', args.simname, args.rundir, 'halo_centric', 'halos_list', f'halos_select_{args.M_around:.1e}_{args.max_halos:d}{args.halos_file_suffix:s}.csv')
 
 outdir = os.path.join(args.outdir, args.simname, args.rundir, 'halo_centric', args.scheme, '{0:d}'.format(args.grid_size) )
 os.makedirs(outdir, exist_ok=True)
