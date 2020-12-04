69e3fb8592bf39c94c03fb13aee8ae5b7ec3e7d5
cprem <premv@iucaa.in>
2020-12-04 11:58:29 +0530

    do both interlaced and without

--- code/global_properties/visualize_simulation.py
+++ code/global_properties/visualize_simulation.py
@@ -10,7 +10,6 @@ import pdb
 
 from gadget_tools import Snapshot
 
-plt.style.use('dark_background')
 
 parser = argparse.ArgumentParser(
     description='Density field evolution from Gadget simulation.',
@@ -22,6 +21,10 @@ parser.add_argument('--rundir', type=str, default='r1',
 
 parser.add_argument('--plots_into', type=str, default='/mnt/home/student/cprem/project-alpha-peak/notes_and_results/plots_and_anims')
 
+parser.add_argument('--snap_i', type=int, default=150, help='Snapshot index number')
+
+parser.add_argument('--light_snaps', type=int, default=0, help='save white bg images for pdf notes')
+
 args = parser.parse_args()
 
 grid_size = 512
@@ -30,6 +33,15 @@ scheme = 'TSC'
 interlaced = True
 
 inlcd_str = '_interlaced' if interlaced==True else ''
+
+if args.light_snaps:
+    theme = '_light'
+    plt.style.use('default')
+    # plt.set_cmap('nipy_spectral')
+else:
+    theme = ''
+    plt.style.use('dark_background')
+    # plt.set_cmap('nipy_spectral')
 
 # simname = 'bdm_cdm1024' if args.simname is None else args.simname
 
@@ -47,12 +59,10 @@ os.makedirs(plotsdir, exist_ok=True)
 plotsdir = os.path.join(args.plots_into, f'{args.simname:s}_{args.rundir:s}', f'full_box_{scheme:s}_{grid_size:d}')
 os.makedirs(plotsdir, exist_ok=True)
 
-i = 150
+i = args.snap_i
 
 with open(os.path.join(infodir, 'header_{0:03d}.p'.format(i)), 'rb') as infofile:
     snap=pickle.load(infofile)
-
-
 
 box_size = snap.box_size
 
@@ -73,17 +83,28 @@ ax1.set_xlabel(r"$h^{-1}$Mpc")
 ax1.set_ylabel(r"$h^{-1}$Mpc")
 # ax1.set_xscale('log')
 
-power_spec = pd.read_csv(os.path.join(Pkdir, 'Pk{1}_{0:03d}.csv'.format(i,inlcd_str)), sep='\t', dtype='float64')
+power_spec = pd.read_csv(os.path.join(Pkdir, 'Pk_{0:03d}.csv'.format(i)), sep='\t', dtype='float64')
 power_spec.columns = ['k', 'Pk']
 
+if interlaced:
+    power_spec_inlcd = pd.read_csv(os.path.join(Pkdir, 'Pk{1}_{0:03d}.csv'.format(i,inlcd_str)), sep='\t', dtype='float64')
+    power_spec_inlcd.columns = ['k', 'Pk']
+
 lin_bin = np.linspace(power_spec['k'].iloc[1],power_spec['k'].iloc[-10], 200)
-log_bin = np.logspace(-2,1.3, 300)
+log_bin = np.logspace(-2,1.3, 100)
+merge_bin = np.concatenate([lin_bin,log_bin])
+merge_bin.sort()
 
 
 power_spec_grouped1 = power_spec.groupby(pd.cut(power_spec['k'], bins=lin_bin)).mean()
+plot2 = ax2.plot(power_spec_grouped1['k'],power_spec_grouped1['Pk'], label="without interlacing")[0]
+
+if interlaced:
+    power_spec_inlcd_grouped1 = power_spec_inlcd.groupby(pd.cut(power_spec_inlcd['k'], bins=lin_bin)).mean()
+    plot3 = ax2.plot(power_spec_inlcd_grouped1['k'],power_spec_inlcd_grouped1['Pk'], label="interlaced")[0]
 # pdb.set_trace()
 
-plot2 = ax2.plot(power_spec_grouped1['k'],power_spec_grouped1['Pk'])[0]
+
 # ax2[1].plot(power_spec['lam'],power_spec['Pk'])
 ax2.set_xlabel(r"$k$  ($h$ Mpc$^{-1}$)")
 ax2.set_ylabel(r"$P(k)$  ($h^{-1}$Mpc)$^3$")
@@ -92,13 +113,13 @@ ax2.set_yscale('log')
 ax2.set_ylim(top=1e5)
 ax2.grid(True)
 ax2.set_title("Power spectrum")
-
+ax2.legend()
 
 plt.tight_layout()
 
-fig1.savefig(os.path.join(plotsdir, 'single_snapshot.pdf'))
-fig1.savefig(os.path.join(plotsdir, 'single_snapshot.png'))
-fig1.savefig(os.path.join(plotsdir, 'single_snapshot.svg'))
+fig1.savefig(os.path.join(plotsdir, f'single_snapshot_{i:03d}{theme:s}.pdf'))
+fig1.savefig(os.path.join(plotsdir, f'single_snapshot_{i:03d}{theme:s}.png'))
+fig1.savefig(os.path.join(plotsdir, f'single_snapshot_{i:03d}{theme:s}.svg'))
 
 def update(i):
     print(i, 'starting')
@@ -111,6 +132,14 @@ def update(i):
     # print(power_spec_grouped1['k'].values)
     plot2.set_xdata(power_spec_grouped1['k'].values)
     plot2.set_ydata(power_spec_grouped1['Pk'].values)
+
+    if interlaced:
+        power_spec_inlcd = pd.read_csv(os.path.join(Pkdir, 'Pk{1}_{0:03d}.csv'.format(i,inlcd_str)), sep='\t', dtype='float64')
+        power_spec_inlcd.columns = ['k', 'Pk']
+        power_spec_inlcd_grouped1 = power_spec_inlcd.groupby(pd.cut(power_spec_inlcd['k'], bins=lin_bin)).mean()
+        # print(power_spec_grouped1['k'].values)
+        plot3.set_xdata(power_spec_inlcd_grouped1['k'].values)
+        plot3.set_ydata(power_spec_inlcd_grouped1['Pk'].values)
 
     with open(os.path.join(infodir, 'header_{0:03d}.p'.format(i)), 'rb') as infofile:
         snap=pickle.load(infofile)
@@ -126,5 +155,6 @@ anim = matplotlib.animation.FuncAnimation(fig1, update, frames=201, interval=500
 Writer=matplotlib.animation.FFMpegWriter
 writer = Writer(fps=10)
 
-anim.save(os.path.join(plotsdir, 'simulation_visualisation.mp4'), writer=writer, dpi=150)
-print("saved")
+if not args.light_snaps:
+    anim.save(os.path.join(plotsdir, 'simulation_visualisation.mp4'), writer=writer, dpi=150)
+    print("saved full box simulation video")
